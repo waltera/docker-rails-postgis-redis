0382a84e83ba034a7474526a69fbec96032613f3
Walter Longo Neto <walterlongoneto@gmail.com>
2022-09-22 14:02:50 -0300

    Removing mandatory bundle install

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,8 +5,5 @@ set -e
 
 cd src
 
-bundle check || bundle install --binstubs="$BUNDLE_BIN"
-# Ensure all gems installed. Add binstubs to bin which has been added to PATH in Dockerfile.
-
 exec "$@"
 # Finally call command issued to the docker service
